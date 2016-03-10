6c649d39770da22565cba356e3c7d7ef2f913869
mnv <mnv@EP890.ephec.be>
2016-03-10 20:51:28 +0100

    MainPersonne pas finie

--- cm2016/src/tp9_/Personne.java
+++ cm2016/src/tp9_/Personne.java
@@ -1,5 +1,24 @@
 package tp9_;
 
-public class Personne {
-
+public class Personne implements Comparable<Personne> {
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	public Personne(String nom, String prenom, Date dateNaissance) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+	}
+	public String toString() {
+		return "Personne [nom=" + nom + ", prenom=" + prenom + ", dateNaissance=" + dateNaissance + "]";
+	}
+	@Override
+	public int compareTo(Personne o) {
+		// TODO Auto-generated method stub
+		return (this.nom+this.prenom).compareToIgnoreCase(
+				o.nom+o.prenom
+		);
+	}
+	
 }
