46e2c328580dad7947600a671fa228fee5d3799b
mnvro <mnv@EP890.ephec.be>
2016-05-28 18:32:28 +0200

    socket

--- cm2016/src/tp17/JFrameJButton.java
+++ cm2016/src/tp17/JFrameJButton.java
@@ -55,10 +55,7 @@ public class JFrameJButton extends JFrame implements ActionListener{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		
 		contentPane.setLayout(gbl_contentPane);
 		for (int i = 0; i < nbLignes; i++) {
 			for (int j= 0; j < nbColonnes; j++) {
